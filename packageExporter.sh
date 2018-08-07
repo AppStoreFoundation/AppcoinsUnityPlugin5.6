9a4a082e087743beb2510a37014345230c44ec55
Nuno Monteiro <nuno.monteir@gmail.com>
2018-08-07 09:52:25 +0100

    Fixed export script
    Exported new package

--- packageExporter.sh
+++ packageExporter.sh
@@ -6,8 +6,8 @@ if [ -z "$UNITY_PATH" ]; then
 fi  
 
 if [ -z "$PROJ_PATH" ]; then
-    echo "Need to set PROJ_PATH! Please run export UNITY_PATH=/path/to/Unity; sh packageExport.sh"
+    echo "Need to set UNITY_PATH! Please run export UNITY_PATH=/path/to/Unity; export PROJ_PATH=/path/to/Unity/Proj; sh packageExport.sh"
     exit 1
 fi  
 
-"$UNITY_PATH" -batchmode -projectPath "$PROJ_PATH" -logFile -quit -executeMethod ExportPackageAutomatically.ExportFromUnity2018
+"$UNITY_PATH" -batchmode -projectPath "$PROJ_PATH" -logFile -quit -executeMethod ExportPackageAutomatically.ExportFromUnity
