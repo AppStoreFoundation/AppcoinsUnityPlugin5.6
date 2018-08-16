4350a77dc42dd023b7ad8541583be2c7e49947c0
Fábio Vital <fabio.vital@aptoide.com>
2018-08-16 13:02:06 +0100

    Fix: Editor dll bugs

--- Appcoins Unity/Assets/AppcoinsUnity/Tools/BashCommand.sh
+++ Appcoins Unity/Assets/AppcoinsUnity/Tools/BashCommand.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-cd '/Users/nunomonteiro/Documents/GitHub/AppcoinsUnityPlugin5_6/Appcoins Unity/and/Appcoins Unity'
-if [ "$('/Users/nunomonteiro/Library/Android/sdk/platform-tools/adb' get-state)" == "device" ]
+cd '/Users/aptoide/Desktop/builds/Appcoins Unity'
+if [ "$('/Users/aptoide/Library/Android/sdk/platform-tools/adb' get-state)" == "device" ]
 then
-'/Users/nunomonteiro/Library/Android/sdk/platform-tools/adb' shell am start -n 'com.aptoide.appcoins/.UnityPlayerActivity' 2>&1 2>'/Users/nunomonteiro/Documents/GitHub/AppcoinsUnityPlugin5_6/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessError.out'
+'/Users/aptoide/Library/Android/sdk/platform-tools/adb' shell am start -n 'com.aptoide.appcoins/com.aptoide.appcoins.UnityPlayerActivity' 2>&1 2>'/Users/aptoide/Documents/GitHub/AppcoinsUnityPlugin5_6/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessError.out'
 else
-echo error: no usb device found > '/Users/nunomonteiro/Documents/GitHub/AppcoinsUnityPlugin5_6/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessError.out'
+echo error: no usb device found > '/Users/aptoide/Documents/GitHub/AppcoinsUnityPlugin5_6/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessError.out'
 fi
-echo 'done' > '/Users/nunomonteiro/Documents/GitHub/AppcoinsUnityPlugin5_6/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessCompleted.out'
+echo 'done' > '/Users/aptoide/Documents/GitHub/AppcoinsUnityPlugin5_6/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessCompleted.out'
 exit
