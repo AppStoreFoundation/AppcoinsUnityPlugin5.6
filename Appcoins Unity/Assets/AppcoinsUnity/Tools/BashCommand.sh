ed6d0baac3c28cda5ffbbc027f29e76a46d566b0
Fábio Vital <fabio.vital@aptoide.com>
2018-08-17 09:58:15 +0100

    dll updates

--- Appcoins Unity/Assets/AppcoinsUnity/Tools/BashCommand.sh
+++ Appcoins Unity/Assets/AppcoinsUnity/Tools/BashCommand.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 cd '/Users/aptoide/Desktop/builds/Appcoins Unity'
-if [ "$('/Users/aptoide/Library/Android/sdk/platform-tools/adb' get-state)" == "device" ]
-then
-'/Users/aptoide/Library/Android/sdk/platform-tools/adb' shell am start -n 'com.aptoide.appcoins/com.aptoide.appcoins.UnityPlayerActivity' 2>&1 2>'/Users/aptoide/Documents/GitHub/AppcoinsUnityPlugin5_6/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessError.out'
-else
-echo error: no usb device found > '/Users/aptoide/Documents/GitHub/AppcoinsUnityPlugin5_6/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessError.out'
-fi
+'/Applications/Android Studio.app/Contents/gradle/gradle-4.4/bin/gradle' assembleDebug 2>&1 2>'/Users/aptoide/Documents/GitHub/AppcoinsUnityPlugin5_6/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessError.out'
 echo 'done' > '/Users/aptoide/Documents/GitHub/AppcoinsUnityPlugin5_6/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessCompleted.out'
 exit
