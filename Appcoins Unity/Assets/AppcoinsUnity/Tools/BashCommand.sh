5add398574614d68152c14562172798c3d982123
Fábio Vital <fabio.vital@aptoide.com>
2018-08-29 13:01:38 +0100

    new dll files and unity package

--- Appcoins Unity/Assets/AppcoinsUnity/Tools/BashCommand.sh
+++ Appcoins Unity/Assets/AppcoinsUnity/Tools/BashCommand.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-cd '/Users/aptoide/Desktop/builds/Appcoins Unity'
-'/Applications/Android Studio.app/Contents/gradle/gradle-4.4/bin/gradle' assembleDebug 2>&1 2>'/Users/aptoide/Documents/GitHub/AppcoinsUnityPlugin5_6/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessError.out'
+cd '/Users/aptoide/Desktop/build/Appcoins Unity'
+'/Applications/Android Studio.app/Contents/gradle/gradle-4.4/bin/gradle' 'assembleRelease' 2>&1 2>'/Users/aptoide/Documents/GitHub/AppcoinsUnityPlugin5_6/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessError.out'
 echo 'done' > '/Users/aptoide/Documents/GitHub/AppcoinsUnityPlugin5_6/Appcoins Unity/Assets/AppcoinsUnity/Tools/ProcessCompleted.out'
 exit
